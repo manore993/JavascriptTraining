a912463a199540b07c11139d5b03662486b16b62
Aishwarya Manore <manore.aishwarya@gmail.com>
2024-06-03 17:12:50 +0200

    Logic written to get computer choice in RockPaperScissor folder.

---
+++ RockPaperScissors/rockpaperscissors.js
@@ -0,0 +1,15 @@
+function getComputerChoice(min, max){
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}
+
+let computerChoice = getComputerChoice(0,2)
+if (computerChoice == 0) {
+    console.log("Rock");
+} else if (computerChoice == 1) {
+    console.log("Paper");
+} else {
+    console.log("Scissor");
+}
+
