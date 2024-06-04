8e0e49a19e652aec8d757c10af161a5ad2369005
Aishwarya Manore <manore.aishwarya@gmail.com>
2024-06-04 20:08:50 +0200

    02_repeatString exercise solved

--- javascript-exercises-main/02_repeatString/repeatString.js
+++ javascript-exercises-main/02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(text, count) {
+    let result = "";
+    if (count < 0){
+        result = 'ERROR';
+    } else {
+        result = text.repeat(count);
+    }
+    return result;
 };
 
 // Do not edit below this line
