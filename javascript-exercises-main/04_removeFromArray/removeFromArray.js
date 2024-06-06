7fdb52c4f9a6a16c4b4d3c6420f81f3e136e6b7c
Aishwarya Manore <manore.aishwarya@gmail.com>
2024-06-06 17:12:47 +0200

    04_removeFromArray solved

--- javascript-exercises-main/04_removeFromArray/removeFromArray.js
+++ javascript-exercises-main/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
 const removeFromArray = function() {
+    let array = arguments[0];
+    // console.log("array " + array);
+    // console.log("arguments " + arguments);
+    
+    for (let i = 1; i < arguments.length; i++){
+        
+        console.log("arguments[i] " + arguments[i]);
+        value = arguments[i];
+        // console.log("value " + value);
+        let j = 0;
+        while (j < array.length){
+            // console.log("array[j] " + array[j]);
+            if (array[j] === value){
+                array.splice(j,1);
+            } else {
+                ++j;
+            }
+        }
+    }
+    return array;
 };
-
+// console.log(removeFromArray([1, 2, 2, 3], 2));
 // Do not edit below this line
 module.exports = removeFromArray;
