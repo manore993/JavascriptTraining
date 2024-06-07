c9e1d3e0505a3f794625d6270c4b915b346fd9d6
Aishwarya Manore <manore.aishwarya@gmail.com>
2024-06-07 16:50:46 +0200

    10_fibonacci solved

--- javascript-exercises-main/10_fibonacci/fibonacci.js
+++ javascript-exercises-main/10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(positionNumber) {
+
+if (Number(positionNumber) >=0) {
+    let fibonacci=[0,1];
+    for (let i=2; i<=positionNumber; i++){
+    fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+    }
+    return fibonacci[positionNumber];
+    // console.log(fibonacci[positionNumber]);
+} else {
+    return "OOPS";
+}
 
 };
-
+// fibonacci(4);
 // Do not edit below this line
 module.exports = fibonacci;
