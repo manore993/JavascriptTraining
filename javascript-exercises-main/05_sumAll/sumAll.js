5b53757d188cafa140b5416bc14e9866251266c5
Aishwarya Manore <manore.aishwarya@gmail.com>
2024-06-07 09:46:10 +0200

    05_summAll solved

--- javascript-exercises-main/05_sumAll/sumAll.js
+++ javascript-exercises-main/05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(limit1, limit2) {
+    // return typeof limit1;
+if ((typeof limit1 != "number") || (typeof limit2 != "number") || ((limit1 || limit2) < 0)) {
+    return "ERROR";
+} else {
+    let sum = 0;
+    if (limit1 < limit2) {
+       for (i=limit1; i<=limit2; i++){
+           sum = sum + i;
+        }
+    } else {
+       for (i=limit1; i>=limit2; i--){
+           sum = sum + i;
+        }
+    } 
+    return sum;
+   };  
+}
+ 
+// console.log(sumAll(1, 4)); 
 // Do not edit below this line
 module.exports = sumAll;
