97207bcae1d58651766072ad342e2f749e6d5422
Aishwarya Manore <manore.aishwarya@gmail.com>
2024-06-07 11:50:47 +0200

    08_calculator

--- javascript-exercises-main/08_calculator/calculator.js
+++ javascript-exercises-main/08_calculator/calculator.js
@@ -1,26 +1,40 @@
-const add = function() {
-	
+const add = function(number1 , number2) {
+	return number1 + number2;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1 , number2) {
+	return number1 - number2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	for (let i=0; i<array.length; i++){
+    sum = sum + array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+	for (let i=0; i<array.length; i++){
+    product = product * array[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	return Math.pow(number, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let factorial = 1;
+  for (let i=1; i<=number; i++){
+    factorial = factorial * i;
+  }
+  return factorial;
 };
+
+console.log(factorial(0));
 
 // Do not edit below this line
 module.exports = {
