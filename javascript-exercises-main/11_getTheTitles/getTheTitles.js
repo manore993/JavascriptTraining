7f517ad57b7b5cc6f8142e24070d45841029d1f0
Aishwarya Manore <manore.aishwarya@gmail.com>
2024-06-07 17:30:14 +0200

    11_getTheTitles (part of objects) solved.

--- javascript-exercises-main/11_getTheTitles/getTheTitles.js
+++ javascript-exercises-main/11_getTheTitles/getTheTitles.js
@@ -1,6 +1,25 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    // console.log(array[0])
+    let arrayOut = [];
+    for (let i=0; i<array.length; i++){
+        let object = array[i];
+        console.log(object);
+        arrayOut[i] = object.title;
+    }
+    return arrayOut;
 };
+
+// let books = [
+//     {
+//       title: 'Book',
+//       author: 'Name'
+//     },
+//     {
+//       title: 'Book2',
+//       author: 'Name2'
+//     }
+// ];
+// console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
