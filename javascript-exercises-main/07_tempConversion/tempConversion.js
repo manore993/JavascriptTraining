a6ef401e2e2b329f6749ac0203b9081d7390e897
Aishwarya Manore <manore.aishwarya@gmail.com>
2024-06-07 11:19:41 +0200

    07_tempConversion solved.

--- javascript-exercises-main/07_tempConversion/tempConversion.js
+++ javascript-exercises-main/07_tempConversion/tempConversion.js
@@ -1,8 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * (5/9);
+  if (celsius % 1 == 0){
+    return celsius;
+  } else {
+    return Math.round(celsius * 10) / 10;
+  }
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = (celsius* (9/5)) + 32 ;
+  if (fahrenheit % 1 == 0){
+    return fahrenheit;
+  } else {
+    return Math.round(fahrenheit * 10) / 10;
+  }
 };
+
+// console.log(convertToCelsius(100));
 
 // Do not edit below this line
 module.exports = {
