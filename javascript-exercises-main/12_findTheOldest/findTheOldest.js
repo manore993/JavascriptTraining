e14de456338894fc18e15c8e90256da8b88276cd
Aishwarya Manore <manore.aishwarya@gmail.com>
2024-06-09 11:53:39 +0200

    12_findTheOldest solved first question with the finding the age method.

--- javascript-exercises-main/12_findTheOldest/findTheOldest.js
+++ javascript-exercises-main/12_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let arrayOfAges = [];
+    for (let i=0; i<array.length; i++){
+        let person = array[i];
+        // console.log(object);
+        arrayOfAges[i] = person.yearOfDeath - person.yearOfBirth;
+    }
+    console.log(arrayOfAges);
+    console.log(Math.max(...arrayOfAges));
+    let oldestPerson = array[arrayOfAges.indexOf(Math.max(...arrayOfAges))]
+    return (oldestPerson);
 };
+
+// const people = [
+//     {
+//       name: "Carly",
+//       yearOfBirth: 1942,
+//       yearOfDeath: 1970,
+//     },
+//     {
+//       name: "Ray",
+//       yearOfBirth: 1962,
+//       yearOfDeath: 2011,
+//     },
+//     {
+//       name: "Jane",
+//       yearOfBirth: 1912,
+//       yearOfDeath: 1941,
+//     }
+// ];
+// console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
