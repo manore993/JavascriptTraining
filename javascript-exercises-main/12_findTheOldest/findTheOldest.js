13a616a67ffd202f3fe16603b9ba0a377451c57a
Aishwarya Manore <manore.aishwarya@gmail.com>
2024-06-10 09:53:34 +0200

    12_findTheOldest solved.

--- javascript-exercises-main/12_findTheOldest/findTheOldest.js
+++ javascript-exercises-main/12_findTheOldest/findTheOldest.js
@@ -1,9 +1,25 @@
 const findTheOldest = function(array) {
-    let arrayOfAges = [];
+    let arrayYearOfBirth = [];
     for (let i=0; i<array.length; i++){
         let person = array[i];
-        // console.log(object);
+        // console.log(person.yearOfDeath);
+        arrayYearOfBirth[i] = person.yearOfBirth;
+    }    
+    let oldestPersonbyYearOfBirth = array[arrayYearOfBirth.indexOf(Math.min(...arrayYearOfBirth))];
+    if (oldestPersonbyYearOfBirth.yearOfDeath === undefined) {
+        return (oldestPersonbyYearOfBirth);
+    }
+    
+    let arrayOfAges = [];
+    
+    for (let i=0; i<array.length; i++){
+        let person = array[i];
+        // console.log(person.yearOfDeath);
         arrayOfAges[i] = person.yearOfDeath - person.yearOfBirth;
+        if (person.yearOfDeath === undefined) {
+            arrayOfAges[i] = 0;
+            // console.log("yod undefined");
+        }
     }
     console.log(arrayOfAges);
     console.log(Math.max(...arrayOfAges));
@@ -14,8 +30,7 @@ const findTheOldest = function(array) {
 // const people = [
 //     {
 //       name: "Carly",
-//       yearOfBirth: 1942,
-//       yearOfDeath: 1970,
+//       yearOfBirth: 1066,
 //     },
 //     {
 //       name: "Ray",
@@ -26,8 +41,8 @@ const findTheOldest = function(array) {
 //       name: "Jane",
 //       yearOfBirth: 1912,
 //       yearOfDeath: 1941,
-//     }
-// ];
+//     },
+//   ]
 // console.log(findTheOldest(people));
 
 // Do not edit below this line
