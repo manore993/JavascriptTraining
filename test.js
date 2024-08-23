985514a7f6ce073dd32530382b51dd38cf3efe49
Aishwarya Manore <manore.aishwarya@gmail.com>
2024-08-23 09:59:45 +0200

    Read csv file javascript

--- test.js
+++ test.js
@@ -1,1 +1,28 @@
-console.log("Hello, World from!")
+// console.log("Hello, World from!")
+
+
+const fs = require("fs");
+const { parse } = require("csv-parse");
+const { delimiter } = require("path");
+
+var buy_amount = 0
+var sell_amount = 0
+
+fs.createReadStream("csv-demo/input.csv")
+    .pipe(parse({ delimiter: ";", from_line:2}))
+    .on("data", function (row) {
+        console.log("--- debut --");
+        console.log(row);
+        if (row[3].trim()=='Buy'){
+            buy_amount -= row[1]*row[2];
+        }
+        if (row[3].trim()=='Sell'){
+            sell_amount += row[1]*row[2];
+        }
+        console.log("--- fin --");
+    })
+    .on('end', function(){
+        console.log(buy_amount);
+        console.log(sell_amount);
+        console.log(sell_amount + buy_amount);
+    })
